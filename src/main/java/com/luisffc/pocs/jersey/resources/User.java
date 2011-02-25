65925b1a45a22f04508b52f552ec0e0098d551fa
Luis Cavalcante <luis_rw@yahoo.com.br>
2011-02-25 16:34:49 -0300

    Criando exemplo de recurso

---
+++ src/main/java/com/luisffc/pocs/jersey/resources/User.java
@@ -0,0 +1,15 @@
+package com.luisffc.pocs.jersey.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/users")
+public class User {
+
+	@GET
+	@Produces("application/xml")
+	public String list(){
+		return "<usuarios><usuario>aee</usuario></usuarios>";
+	}
+}
